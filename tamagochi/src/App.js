d4dc624d8ca814af852846295ea50a30df4b76e9
Matteo <150077808+mballuais@users.noreply.github.com>
2024-10-01 15:04:56 +0200

    Update App.js

--- tamagochi/src/App.js
+++ tamagochi/src/App.js
@@ -1,11 +1,27 @@
 import React, { useState } from 'react';
 import Button from './components/atoms/Button';
-import ProgressBar from './components/atoms/ProgessBar';
+import ProgressBar from './components/atoms/ProgressBar';
 import EvolutionImage from './components/assets/images';
+import TamagotchiPanel from './components/organisms/TamagotchiPanel'
 
 // function App() {
 //   const [count, setCount] = useState(0);
 
+const App = () => {
+  const [progress, setProgress] = useState(0);
+
+
+  // const handleClick = () => {
+  //   setCount(count + 1);
+  // };
+
+  const increaseProgress = () => {
+    setProgress(prevProgress => Math.min(prevProgress + 10, 100)); // Incrémente de 10% sans dépasser 100%
+  };
+
+
+
+  
 const App = () => {
   const [progress, setProgress] = useState(0);
 
@@ -35,6 +51,7 @@ const App = () => {
       <button onClick={increaseProgress} style={{ marginTop: '10px' }}>
         Increase Progress
       </button>
+      <TamagotchiPanel />
     </div>
   );
 }
